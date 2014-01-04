f54326eec50bfdb6bf652e7d7bed55b8b2b6dc59
Malgorzata Migdal <malgorzata@migdal.pl>
2014-01-04 10:14:57 -0800

    Find freee spots in calendar

--- js/application.js
+++ js/application.js
@@ -5,18 +5,49 @@ $(function () {
         googleScope: "https://www.googleapis.com/auth/calendar.readonly"
     };
 
+    var findCalendarIds = function () {
+        console.log("Authorized access to Google calendar")
+        gapi.client.request({
+            path: "calendar/v3/users/me/calendarList",
+            callback: findFreeSpots
+        });
+    };
+
+    var findFreeSpots = function (data) {
+        console.log("Got calendar ids");
+        var primaryId = "";
+        for (var i = 0 ; i < data.items.length ; i++) {
+            var item = data.items[i];
+            if (item.primary == true) {
+                primaryId = item.id;
+            };
+        };
+        console.log("Id of primary calendar: " + primaryId);
+        gapi.client.request({
+            path: "calendar/v3/freeBusy",
+            method: "POST",
+            body: {
+                timeMin: "2014-01-03T10:00:00Z",
+                timeMax: "2014-01-10T10:00:00Z",
+                items: [{id: primaryId}]
+            },
+            callback: findEvents
+        });
+    };
+
+    var findEvents = function (data) {
+        console.log("Find events for free spots.");
+    };
+
     $("#find-events").click(function (event) {
         console.log("Finding events");
         gapi.client.setApiKey(config.googleApiKey);
         gapi.auth.authorize(
             {client_id: config.googleClientId, scope: config.googleScope},
-            function () {
-                console.log("authorized access to Google calendar")
-            }
+            findCalendarIds
         );
     });
 });
-
 
 
 
