fa8c352f32f4d13437b80a41d666367ef6dbc3bd
Malgorzata Migdal <malgorzata@migdal.pl>
2014-01-03 18:35:53 -0800

    Allow access to the calendar

--- js/application.js
+++ js/application.js
@@ -1,6 +1,21 @@
-$(function() {
+$(function () {
+    var config = {
+        googleApiKey: "AIzaSyDrKH9w5_wCjizggLncPp215z1OmCEHlFU",
+        googleClientId: "552663294103-925qfrkdt8bfnqe80q4st9pssi0t0g01.apps.googleusercontent.com",
+        googleScope: "https://www.googleapis.com/auth/calendar.readonly"
+    };
 
-})
+    $("#find-events").click(function (event) {
+        console.log("Finding events");
+        gapi.client.setApiKey(config.googleApiKey);
+        gapi.auth.authorize(
+            {client_id: config.googleClientId, scope: config.googleScope},
+            function () {
+                console.log("authorized access to Google calendar")
+            }
+        );
+    });
+});
 
 
 
