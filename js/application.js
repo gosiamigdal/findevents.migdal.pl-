cc3b6d435dbe77de6f6323e34f9b4d8d562a3088
Malgorzata Migdal <malgorzata@migdal.pl>
2014-01-04 11:06:57 -0800

    Get all events, show just 10

--- js/application.js
+++ js/application.js
@@ -2,8 +2,18 @@ $(function () {
     var config = {
         googleApiKey: "AIzaSyDrKH9w5_wCjizggLncPp215z1OmCEHlFU",
         googleClientId: "552663294103-925qfrkdt8bfnqe80q4st9pssi0t0g01.apps.googleusercontent.com",
-        googleScope: "https://www.googleapis.com/auth/calendar.readonly"
+        googleScope: "https://www.googleapis.com/auth/calendar.readonly",
+        eventbriteApiKey: "FCD3JWTG53R3SD3FFC"
     };
+
+    $("#find-events").click(function (event) {
+        console.log("Finding events");
+        gapi.client.setApiKey(config.googleApiKey);
+        gapi.auth.authorize(
+            {client_id: config.googleClientId, scope: config.googleScope},
+            findCalendarIds
+        );
+    });
 
     var findCalendarIds = function () {
         console.log("Authorized access to Google calendar")
@@ -15,6 +25,7 @@ $(function () {
 
     var findFreeSpots = function (data) {
         console.log("Got calendar ids");
+        // TODO: Ask user which calendar to use
         var primaryId = "";
         for (var i = 0 ; i < data.items.length ; i++) {
             var item = data.items[i];
@@ -23,12 +34,15 @@ $(function () {
             };
         };
         console.log("Id of primary calendar: " + primaryId);
+        var now = new Date();
+        var weekLater = new Date();
+        weekLater.setDate(now.getDate() + 7);
         gapi.client.request({
             path: "calendar/v3/freeBusy",
             method: "POST",
             body: {
-                timeMin: "2014-01-03T10:00:00Z",
-                timeMax: "2014-01-10T10:00:00Z",
+                timeMin: now.toISOString(),
+                timeMax: weekLater.toISOString(),
                 items: [{id: primaryId}]
             },
             callback: findEvents
@@ -37,16 +51,22 @@ $(function () {
 
     var findEvents = function (data) {
         console.log("Find events for free spots.");
+        $.ajax({
+            type: "GET",
+            url: "https://developer.eventbrite.com/json/event_search",
+            dataType: "jsonp",
+            data: {
+                app_key: config.eventbriteApiKey//,
+                //max: 100,
+                //date: "Next week"
+            },
+            success: function (msg) {
+                console.log("Events found");
+            }
+        });
     };
 
-    $("#find-events").click(function (event) {
-        console.log("Finding events");
-        gapi.client.setApiKey(config.googleApiKey);
-        gapi.auth.authorize(
-            {client_id: config.googleClientId, scope: config.googleScope},
-            findCalendarIds
-        );
-    });
+
 });
 
 
